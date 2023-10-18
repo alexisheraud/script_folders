fe99d3edaf840dde6852d46d0493a0ec3dfa2e78
alexis.heraud <alexis.heraud@epitech.eu>
2023-10-18 13:24:28 +0200

    [MODIFIED] - script.sh | Add the functionnality to create a test.c when u say ./script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if (($# < 1)); then
-    echo "Usage: $0 [HELP]";
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [HELP]"
+    exit 1
 fi
 
 if [ "$1" = "help" ] || [ "$1" = "HELP" ]; then
-    cat help.txt;
+    cat help.txt
     echo
+fi
+
+if [ "$1" = "." ]; then
+    touch "test.c"
+    echo "Fichier 'test.c' créé dans le répertoire courant : $PWD"
 fi
