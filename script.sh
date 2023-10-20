4d8603d05e42f342bd966709eadb7883e7192c89
alexis.heraud <alexis.heraud@epitech.eu>
2023-10-20 16:38:34 +0200

    [MODIFIED] - script.sh | Added red message when error encounter & added the makefile creation alongside with the main

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 [HELP]"
+    echo -e "\e[91mUsage: $0 [HELP]"
+    echo -e "\e[0m"
     exit 1
 fi
 
@@ -10,7 +12,28 @@ if [ "$1" = "help" ] || [ "$1" = "HELP" ]; then
     echo
 fi
 
-if [ "$1" = "." ]; then
-    touch "test.c"
-    echo "Fichier 'test.c' créé dans le répertoire courant : $PWD"
+if [ $# -eq 1 ] && [ "$1" = "." ]; then
+    main_file="main.c"
+
+    echo "#include <stdio.h>
+
+int main(void) {
+    printf(\"Hello, World!\\n\");
+    return 0;
+}" > "$main_file"
+
+    echo "Fichier '$main_file' créé dans le répertoire courant : $PWD"
+
+    make_file="makefile"
+
+echo "test" > "$make_file"
+
+    echo "Fichier '$make_file' créé dans le répertoire courant : $PWD"
+
+elif [ $# -eq 1 ] && [ "$1" != "help" ] && [ "$1" != "HELP" ]; then
+    echo -e "\e[91mErreur : Argument incorrect. Utilisation : $0 [.]"
+    echo -e "\e[0m"  # Réinitialisation de la couleur
+elif [ $# -eq 0 ]; then
+    echo -e "\e[91mErreur : Aucun argument spécifié. Utilisation : $0 [.]"
+    echo -e "\e[0m"  # Réinitialisation de la couleur
 fi
